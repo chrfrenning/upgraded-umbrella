ab9da35fe2df05b84ac5f24096d63b9f56d60ced
alina.lapina <alina.lapina@bidbax.no>
2023-09-13 03:34:27 +0200

    Use Logger in the network simulator

--- src/NetworkSimulator.java
+++ src/NetworkSimulator.java
@@ -1,10 +1,13 @@
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.logging.*;
 
 public class NetworkSimulator {
+    private static final Logger LOGGER = Logger.getLogger(NetworkSimulator.class.getName());
 
     private static final int AMOUNT_OF_ZONES = 5;
     private static final int PORT = 1099;
+
     /**
      * The method creates a registry.
      * THen it binds an instance of a load balancer and a few instances of server stubs.
@@ -15,14 +18,14 @@ public class NetworkSimulator {
             Registry registry = LocateRegistry.createRegistry(PORT);
             // Bind the load balancer to the registry
             registry.bind("LoadBalancer", new LoadBalancer());
-            System.out.println("LoadBalancer is ready.");
+            LOGGER.info("LoadBalancer is ready.");
             // Bind remote objects to the registry
             for (int zone = 1; zone <= AMOUNT_OF_ZONES; zone++) {
                 registry.bind(String.valueOf(zone), new Server(zone));
-                System.out.printf("Server in zone %d is registered.%n", zone);
+                LOGGER.info(String.format("Server in zone %d is registered.%n", zone));
             }
         } catch (Exception e) {
-            System.err.println("Cannot register load balancer or servers.");
+            LOGGER.severe("Cannot register load balancer or servers.");
             e.printStackTrace();
         }
     }
