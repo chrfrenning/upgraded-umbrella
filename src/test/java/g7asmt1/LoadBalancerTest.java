7a4a336da8ca7064ca9523ac16ea80696cc23b4a
alina.lapina <alina.lapina@bidbax.no>
2023-09-16 14:18:37 +0200

    #16 Upgrade junit version, first test method

---
+++ src/test/java/g7asmt1/LoadBalancerTest.java
@@ -0,0 +1,13 @@
+package g7asmt1;
+
+import org.junit.jupiter.api.Test;
+
+public class LoadBalancerTest {
+    public LoadBalancerTest() {
+    }
+
+    @Test
+    void test() {
+        assert ( true );
+    }
+}
